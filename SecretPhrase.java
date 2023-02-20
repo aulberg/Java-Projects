4e79e6c1a1744ae636a8773a92484870dd5d971a
Ava Ulberg <76458102+aulberg@users.noreply.github.com>
2023-02-20 12:10:23 +0000

    secret phrases change

--- SecretPhrase.java
+++ SecretPhrase.java
@@ -1,55 +1,44 @@
-import java.util.Scanner; 
-import java.util.Random; 
-public class SecretPhrase {
-    public static void main(String[] args) {
-    
-    String phrase[] = {"blue in the face", "ignorance is bliss", 
-        "my archilles heel", "hold your horses", "the worlds my oyster", 
-        "point of no return", "shiver me timbers", "tell it to the judge", 
+import java.util.Scanner;  
+public class SecretPhrase {  
+   
+    private static String[] phrases = {"blue in the face", "ignorance is bliss",
+        "my archilles heel", "hold your horses", "the worlds my oyster",
+        "point of no return", "shiver me timbers", "tell it to the judge",
         "dont beat a dead horse", "no man is an island"};
-    boolean isFinished = false;
-    
-    Random random = new Random();
-    Scanner input = new Scanner(System.in);
-    
-    //users phrase withoout astericks 
-    int index = random.nextInt(phrase.length);
-    String userPhrase = phrase[index];
-    System.out.println(userPhrase);
-    
-    //usersphrase with astericks
-    String userSecretPhrase = userPhrase.replaceAll("[a-zA-Z]", "*");
-    
-    //turns the users phrase into a char array
-    char[] textArray = userPhrase.toCharArray(); 
-    
-    //used to compare the letters the user enters to what is in the textArray
-    char[] answers = new char[textArray.length];
-    for(int i = 0; i < textArray.length; ++i) {
-        answers[i] = '?';
-    }
-    
-    
-    while(isFinished = false) {
-        String letter = input.next();
-        
-        while(letter.length() != 1 || Character.isDigit(letter.charAt(0))) {
-            System.out.println("Invalid Input: Re-enter letter");
-            letter = input.next();
-        }
-        
-        boolean found = false; 
-        for(int i = 0; i < textArray.length; ++i) {
-            if(letter.charAt(0) == textArray[i]) {
-                answers[i] = textArray[i];
-                found = true; 
-            }
-        }
-        
-        if(!found) {
-            System.out.println("Wrong letter");
-        }
-        
-    }
+private static String word = phrases[(int) (Math.random() * phrases.length)];
+private static String secretPhrase = new String(new char[word.length()]).replace("\0", "*");
+private static int counter = 0;
+
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+
+while (secretPhrase.contains("*")) {
+System.out.println("Guess any letter in the phrase");
+System.out.println(secretPhrase);
+String playerGuess = input.next();
+buildPhrase(playerGuess);
+}
+}
+
+public static void buildPhrase(String playerGuess) {
+String newPhrase = "";
+for (int i = 0; i < word.length(); i++) {
+if (word.charAt(i) == playerGuess.charAt(0)) {
+newPhrase += playerGuess.charAt(0);
+} else if (secretPhrase.charAt(i) != '*') {
+newPhrase += word.charAt(i);
+} else {
+newPhrase += "*";
+}
+}
+
+if (secretPhrase.equals(newPhrase)) {
+counter++;
+} else {
+secretPhrase = newPhrase;
+}
+if (secretPhrase.equals(word)) {
+System.out.println("You guessed the correct phrase!");
+}
 }
 }
